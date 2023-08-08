68b80c874187abb51b552516e9a738a6faae4ed2
jeandedieu <jeandedieusessou@gmail.com>
2023-08-08 10:34:43 +0000

    les variables

---
+++ partie1/index.js
@@ -0,0 +1,7 @@
+// let nom declaration de variable avec let
+// const pour declarer le constante 
+// prompt pour demandé a l'utilisateur une saisie 
+ let nom = prompt("quel est votre nom") ;
+ let prenom = prompt("quel est votre prenom");
+
+ console.log(nom, prenom) ;
