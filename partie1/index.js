3210ac52b7046ef55b9db2c498c5c933e9d56ad2
jeandedieu <jeandedieusessou@gmail.com>
2023-08-08 11:07:43 +0000

    deuxieume sauvegarde(les conditions)

--- partie1/index.js
+++ partie1/index.js
@@ -1,7 +1,16 @@
 // let nom declaration de variable avec let
 // const pour declarer le constante 
-// prompt pour demandé a l'utilisateur une saisie 
- let nom = prompt("quel est votre nom") ;
+// prompt pour demandé a l'utilisateur une saisie (il recoit que des chaines de caractere car java est faiblement typé)
+
+/*  let nom = prompt("quel est votre nom") ;
  let prenom = prompt("quel est votre prenom");
 
- console.log(nom, prenom) ;
+ console.log(nom, prenom) ; */
+
+ let nombre = prompt("Veuillez choisir un nombre");
+
+if (nombre > 0) {
+    console.log("positif");
+} else {
+    console.log("negatif");
+}
