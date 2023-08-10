1708ae97ae50facdb455d6925ac4ddac67fc7f7a
jeandedieu <jeandedieusessou@gmail.com>
2023-08-10 13:49:25 +0000

    correction du petit jeu avec OOP

--- partie3/index.js
+++ partie3/index.js
@@ -12,26 +12,65 @@ class Personnage{
         console.log(`je m'appelle ${this.nom}`);
     }
 
-    
-    attaque(Personnage, arme){
-        for(let element of this.arme){
-            if(arme==element.arme){
-                Personnage.nbreVie = Personnage.nbreVie - element.degat ;
+/*     attaque(Personnage, arme){
+        if(arme==this.arme[0].arme) {
+            Personnage.nbreVie = Personnage.nbreVie - this.arme[0].degat;
+            console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+            
+            while(Personnage.nbreVie!=0){
+                Personnage.nbreVie = Personnage.nbreVie - this.arme[0].degat;
                 console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+                if(Personnage.nbreVie<=0){
+                    this.exp = this.exp + 10 ;
+                    console.log(`${this.nom} a vaincu ${Personnage.nom} il a maintenant ${this.exp} exp`);
+                }
+            }
 
-                while(Personnage.nbreVie!=0 && Personnage.nbreVie >0){
-                    Personnage.nbreVie = Personnage.nbreVie - element.degat;
+        }else if(arme==this.arme[1].arme){
+            Personnage.nbreVie = Personnage.nbreVie - this.arme[1].degat;
+            console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+            
+            while(Personnage.nbreVie!=0){
+                Personnage.nbreVie = Personnage.nbreVie - this.arme[1].degat;
+                console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+                if(Personnage.nbreVie<=0){
+                    this.exp = this.exp + 10 ;
+                    console.log(`${this.nom} a vaincu ${Personnage.nom} il a maintenant ${this.exp} exp`);
+                }
+            }
 
-                    console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+        }else if(arme==this.arme[2].arme){
+            Personnage.nbreVie = Personnage.nbreVie - this.arme[2].degat;
+            console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
 
-                    if(Personnage.nbreVie<=0){
-                        this.exp = this.exp + 10 ;
-                        console.log(`${this.nom} a vaincu ${Personnage.nom} il a maintenant ${this.exp} exp`);
-                    }
+            while(Personnage.nbreVie!=0){
+                Personnage.nbreVie = Personnage.nbreVie - this.arme[2].degat;
+                console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+                if(Personnage.nbreVie<=0){
+                    this.exp = this.exp + 10 ;
+                    console.log(`${this.nom} a vaincu ${Personnage.nom} il a maintenant ${this.exp} exp`);
                 }
             }
         }
-    }
+    } */
+    
+     attaque(Personnage, arme){
+         while(Personnage.nbreVie!=0 && Personnage.nbreVie >0){
+            for(let element of this.arme){
+                if(arme==element.arme){
+                Personnage.nbreVie = Personnage.nbreVie - element.degat ;
+                console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+
+                    Personnage.nbreVie = Personnage.nbreVie - element.degat;
+                    console.log(`${this.nom} a attaque ${Personnage.nom} avec ${arme}. il lui reste ${Personnage.nbreVie} point de vie`);
+                }
+            }
+            if(Personnage.nbreVie<=0){
+                this.exp = this.exp + 10 ;
+                console.log(`${this.nom} a vaincu ${Personnage.nom} il a maintenant ${this.exp} exp`);
+            }
+        }
+    } 
 };
 
 let luffy = new Personnage("luffy");
